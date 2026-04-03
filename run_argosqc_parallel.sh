4705746bdc30798ffe2c4b94a91fc6b3e8b9770c
yingniu <yinghuan.niu@shaw.ca>
2026-04-03 17:11:18 -0600

    Fixed cron log name

--- run_argosqc_parallel.sh
+++ run_argosqc_parallel.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /opt/otn_nrt/rt-sat-to-obis &&  python py_nrt/run_argosqc_parallel.py >> /var/log/argosqc_cron.log 2>&1
+cd /opt/otn_nrt/rt-sat-to-obis &&  python py_nrt/run_argosqc_parallel.py >> /var/log/otn_nrt_pipeline_cron.log 2>&1
---
+++ run_load_today_ssmoutput.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cd /opt/otn_nrt/rt-sat-to-obis &&  python py_nrt/run_load_today_nrt_results.py >> /var/log/otn_nrt_pipeline_cron.log 2>&1
