5dd1acdaa161018a5365b83fec4388270f76741c
yingniu <yinghuan.niu@shaw.ca>
2026-04-03 17:48:42 -0600

    Fixed python path

--- run_argosqc_parallel.sh
+++ run_argosqc_parallel.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-cd /opt/otn_nrt/rt-sat-to-obis && /opt/miniconda3/bin/conda init
-cd /opt/otn_nrt/rt-sat-to-obis && /opt/miniconda3/bin/conda activate rt-sat-to-obis
-cd /opt/otn_nrt/rt-sat-to-obis &&  python py_nrt/run_argosqc_parallel.py >> otn_nrt_pipeline_cron.log 2>&1
+cd /opt/otn_nrt/rt-sat-to-obis &&  /opt/miniconda3/envs/rt-sat-to-obis/bin/python py_nrt/run_argosqc_parallel.py >> otn_nrt_pipeline_cron.log 2>&1
