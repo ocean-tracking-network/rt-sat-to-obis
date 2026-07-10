0c9525ac8e6496e75fb804ef4e4727f665e9b208
yingniu <yinghuan.niu@shaw.ca>
2026-07-09 21:30:11 -0600

    Fixed show_smru_login

--- py_nrt/argos_smru_project_config.py
+++ py_nrt/argos_smru_project_config.py
@@ -133,17 +133,20 @@ def show_smru_login(qc_input_path, program, upload_mode, cid, collectioncode) ->
         display(HTML('<h3 style="margin: 0; color: blue;">Near real-time mode require SMRU login credentials:</h3>'))
         return show_user_passwd_textboxes()
     elif upload_mode == 'delay':
-        display(HTML(f'<h3 style="margin: 0; color: blue;">Delay mode requires upload <cid>.mdb file to below folder:</h3>'))
         # base_url = "https://jphub.oceantrack.org/user/satnrt/notebooks/rt-sat-to-obis"
         base_url = "http://localhost:8888/tree"
         current_dir = os.path.dirname(__file__)
-        folder_path = os.path.join(os.path.dirname(current_dir), qc_input_path, program, f'{program}_{cid}', 'mdb')
+        relative_path = os.path.join(qc_input_path, program, f'{program}_{cid}', 'mdb')
+        folder_path = os.path.join(os.path.dirname(current_dir), relative_path)
+        if not os.path.exists(os.path.join(folder_path, f'{cid}.mdb')):
+            print(f'Delay mode requires user to upload {cid}.mdb file to below folder: {relative_path}')
+            if not os.path.exists(folder_path):
+                os.makedirs(folder_path)
+            upload_url = os.path.join(base_url, qc_input_path, program, f'{program}_{cid}', 'mdb')
+            display(HTML(f'<a href="{upload_url}" target="_blank">{relative_path}</a>'))
+        else:
+            print(f'Delay mode found {cid}.mdb file to below folder: {relative_path}')
 
-        if not os.path.exists(folder_path):
-            os.makedirs(folder_path)
-            print(f'Created project configuration folder: {folder_path}')
-        upload_url = os.path.join(base_url, qc_input_path, program, f'{program}_{cid}', 'mdb')
-        display(HTML(f'<a href="{upload_url}" target="_blank">{folder_path}</a>'))
         return None, None
 
 
