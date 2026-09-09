3537a22d7fb1c60334cb0e04fdece360b9b6bc20
yingniu <yinghuan.niu@shaw.ca>
2026-09-09 08:06:07 -0600

    Added Create functions to pull satellite tags and animals from otnunit

--- py_nrt/init_sat_tables.sql
+++ py_nrt/init_sat_tables.sql
@@ -116,12 +116,222 @@ CREATE INDEX IF NOT EXISTS idx_sat_deployments_campaign_id
     ON {{sat_schema}}.sat_deployments (campaign_id);
 
 -- ============================================
--- Verify tables were created
+-- Create functions to pull satellite tags and animals from otnunit
 -- ============================================
-SELECT
-    schemaname,
-    tablename,
-    tableowner
-FROM pg_tables
-WHERE schemaname = 'satnrt'
-ORDER BY tablename;
+
+CREATE TABLE IF NOT EXISTS obis.otn_satellite_tags_animals (
+    basisofrecord varchar(256),
+    institutioncode varchar(200),
+    collectioncode varchar(200),
+    catalognumber varchar(200),
+    datelastmodified varchar(200),
+    scientificname varchar(200),
+    commonname varchar,
+    collector varchar(200),
+    datecollected timestamp,
+    timezone varchar(100),
+    yearcollected varchar(8),
+    monthcollected varchar(4),
+    daycollected varchar(4),
+    julianday int4,
+    timeofday float8,
+    latitude float8,
+    longitude float8,
+    coordinateprecision float8,
+    locality text,
+    fieldnumber varchar(100),
+    the_geom public.geometry,
+    stock text,
+    wild_ind text,
+    sex text,
+    observedindividualcount numeric,
+    observedweight numeric,
+    length numeric,
+    lengthtype text,
+    length2 numeric,
+    length2type text,
+    age numeric,
+    ageunit text,
+    lifestage text,
+    lifestagedescription text,
+    datereleaseanimal date,
+    datedetectionsreleased date,
+    notes text,
+    organism_id text,
+    -- All columns from otn_satellite_tags
+    tag_datelastmodified varchar,
+    tag_institutioncode varchar,
+    tag_collectioncode varchar,
+    tag_catalognumber varchar,
+    ptt_code varchar,
+    tag_startdatetime timestamp,
+    tag_enddatetime timestamp,
+    tag_timezone varchar,
+    tag_locality varchar,
+    tag_latitude numeric,
+    tag_longitude numeric,
+    instrumenttype varchar,
+    instrumentmodel varchar,
+    collectornumber varchar,
+    fieldnumber_tag varchar,
+    relationshiptype varchar,
+    relatedcatalogitem varchar,
+    tag_the_geom public.geometry,
+    tag_notes text
+);
+
+
+CREATE OR REPLACE FUNCTION obis.repopulate_satellite_tags(
+    p_username TEXT,
+    p_password TEXT
+)
+RETURNS VOID AS
+$$
+DECLARE
+    v_source_conn TEXT := 'host=db.load.oceantrack.org port=5432 dbname=otnunit user=' || p_username || ' password=' || p_password;
+BEGIN
+    -- Truncate existing tables
+    TRUNCATE TABLE obis.otn_satellite_tags;
+    TRUNCATE TABLE obis.otn_satellite_tags_animals;
+
+    -- Populate otn_satellite_tags
+    INSERT INTO obis.otn_satellite_tags (
+        datelastmodified,
+        institutioncode,
+        collectioncode,
+        catalognumber,
+        ptt_code,
+        startdatetime,
+        enddatetime,
+        timezone,
+        locality,
+        latitude,
+        longitude,
+        instrumenttype,
+        instrumentmodel,
+        collectornumber,
+        fieldnumber,
+        relationshiptype,
+        relatedcatalogitem,
+        the_geom,
+        notes
+    )
+    SELECT
+        datelastmodified,
+        institutioncode,
+        collectioncode,
+        catalognumber,
+        ptt_code,
+        startdatetime::timestamp,
+        enddatetime::timestamp,
+        timezone,
+        locality,
+        latitude,
+        longitude,
+        instrumenttype,
+        instrumentmodel,
+        collectornumber,
+        fieldnumber,
+        relationshiptype,
+        relatedcatalogitem,
+        ST_SetSRID(ST_MakePoint(longitude, latitude), 4326) AS the_geom,
+        notes
+    FROM dblink(
+        v_source_conn,
+        'SELECT datelastmodified, institutioncode, collectioncode, catalognumber, ptt_code, startdatetime, enddatetime, timezone, locality, latitude, longitude, instrumenttype, instrumentmodel, collectornumber, fieldnumber, relationshiptype, relatedcatalogitem, notes FROM obis.otn_satellite_tags'
+    ) AS t (
+        datelastmodified varchar,
+        institutioncode varchar,
+        collectioncode varchar,
+        catalognumber varchar,
+        ptt_code varchar,
+        startdatetime text,
+        enddatetime text,
+        timezone varchar,
+        locality varchar,
+        latitude numeric,
+        longitude numeric,
+        instrumenttype varchar,
+        instrumentmodel varchar,
+        collectornumber varchar,
+        fieldnumber varchar,
+        relationshiptype varchar,
+        relatedcatalogitem varchar,
+        notes text
+    );
+
+    -- Populate otn_satellite_tags_animals
+    INSERT INTO obis.otn_satellite_tags_animals
+    SELECT
+        oa.*,
+        ot.datelastmodified,
+        ot.institutioncode,
+        ot.collectioncode,
+        ot.catalognumber,
+        ot.ptt_code,
+        ot.startdatetime,
+        ot.enddatetime,
+        ot.timezone,
+        ot.locality,
+        ot.latitude,
+        ot.longitude,
+        ot.instrumenttype,
+        ot.instrumentmodel,
+        ot.collectornumber,
+        ot.fieldnumber,
+        ot.relationshiptype,
+        ot.relatedcatalogitem,
+        ot.the_geom,
+        ot.notes
+    FROM dblink(
+        v_source_conn,
+        'SELECT basisofrecord, institutioncode, collectioncode, catalognumber, datelastmodified, scientificname, commonname, collector, datecollected, timezone, yearcollected, monthcollected, daycollected, julianday, timeofday, latitude, longitude, coordinateprecision, locality, fieldnumber, the_geom, stock, wild_ind, sex, observedindividualcount, observedweight, length, lengthtype, length2, length2type, age, ageunit, lifestage, lifestagedescription, datereleaseanimal, datedetectionsreleased, notes, organism_id FROM obis.otn_animals'
+    ) AS oa (
+        basisofrecord varchar(256),
+        institutioncode varchar(200),
+        collectioncode varchar(200),
+        catalognumber varchar(200),
+        datelastmodified varchar(200),
+        scientificname varchar(200),
+        commonname varchar,
+        collector varchar(200),
+        datecollected timestamp,
+        timezone varchar(100),
+        yearcollected varchar(8),
+        monthcollected varchar(4),
+        daycollected varchar(4),
+        julianday int4,
+        timeofday float8,
+        latitude float8,
+        longitude float8,
+        coordinateprecision float8,
+        locality text,
+        fieldnumber varchar(100),
+        the_geom public.geometry,
+        stock text,
+        wild_ind text,
+        sex text,
+        observedindividualcount numeric,
+        observedweight numeric,
+        length numeric,
+        lengthtype text,
+        length2 numeric,
+        length2type text,
+        age numeric,
+        ageunit text,
+        lifestage text,
+        lifestagedescription text,
+        datereleaseanimal date,
+        datedetectionsreleased date,
+        notes text,
+        organism_id text
+    )
+    JOIN obis.otn_satellite_tags ot
+    ON oa.catalognumber = ot.relatedcatalogitem;
+
+EXCEPTION
+    WHEN OTHERS THEN
+        RAISE;
+END;
+$$
+LANGUAGE plpgsql;
