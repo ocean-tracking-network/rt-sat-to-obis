5e75de559a10f4b650ff171dfd65cb6c3f58e651
yingniu <yinghuan.niu@shaw.ca>
2026-09-10 09:21:47 -0600

    Added export telemetry to csv

--- py_nrt/argos_smru_project.py
+++ py_nrt/argos_smru_project.py
@@ -780,6 +780,7 @@ def extract_ssmoutput_tracks(engine: Engine, program: str, cid: str, qc_output_p
         how='left'
     )
     filename = f"{cid}_qced_telemetry_{datetime.now().strftime('%Y%m%d')}.csv"
+    ssmoutputs_df.to_csv(os.path.sep.join([ssmoutput_folder, filename]), index=False)
     display(HTML(f'''<p>
         <span style="font-size:25px;"><i class="fa fa-flip-horizontal">🐟</i></span>
         <span style="font-size:20px;">~ Click CSV button to download and attach "{filename}" to the Gitlab issue.</span>
